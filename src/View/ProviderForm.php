3741c648b9f59434685f5d36d3f2fb4786d0d580
Miika Arponen <miika@geniem.com>
2019-03-25 16:13:06 +0200

    Version 1.0.0-beta

--- src/View/ProviderForm.php
+++ src/View/ProviderForm.php
@@ -19,21 +19,35 @@ if ( ! empty( $data['error'] ) ) {
     return;
 }
 
-echo '<ul class="woocommerce-gateway-checkout-finland-payment-fields">';
+$group_titles = [
+    'mobile'     => __( 'Mobile payment methods', 'woocommerce-payment-gateway-checkout-finland' ),
+    'bank'       => __( 'Bank payment methods', 'woocommerce-payment-gateway-checkout-finland' ),
+    'creditcard' => __( 'Card payment methods', 'woocommerce-payment-gateway-checkout-finland' ),
+    'credit'     => __( 'Invoice and instalment payment methods', 'woocommerce-payment-gateway-checkout-finland' ),
+];
 
-array_walk( $data, function( $provider ) {
-    printf(
-        '<li class="woocommerce-gateway-checkout-finland-payment-fields--list-item">
-            <label>
-                <input class="woocommerce-gateway-checkout-finland-payment-fields--list-item--input" type="radio" name="payment_provider" value="%s">
-                <div class="woocommerce-gateway-checkout-finland-payment-fields--list-item--wrapper">
-                    <img class="woocommerce-gateway-checkout-finland-payment-fields--list-item--img" src="%s">
-                </div>
-            </label>
-        </li>',
-        esc_html( $provider->getId() ),
-        esc_html( $provider->getSvg() )
-    );
+array_walk( $data, function( $provider_group, $title ) use ( $group_titles ) {
+
+    echo '<h4>' . esc_html( $group_titles[ $title ] ?? $title ) . '</h4>';
+
+    echo '<ul class="woocommerce-gateway-checkout-finland-payment-fields">';
+    array_walk( $provider_group, function( $provider ) {
+        printf(
+            '<li class="woocommerce-gateway-checkout-finland-payment-fields--list-item">
+                <label>
+                    <input
+                        class="woocommerce-gateway-checkout-finland-payment-fields--list-item--input"
+                        type="radio" name="payment_provider" value="%s">
+                    <div class="woocommerce-gateway-checkout-finland-payment-fields--list-item--wrapper">
+                        <img class="woocommerce-gateway-checkout-finland-payment-fields--list-item--img" src="%s">
+                    </div>
+                </label>
+            </li>',
+            esc_html( $provider->getId() ),
+            esc_html( $provider->getSvg() )
+        );
+    });
+
+    echo '</ul>';
 });
 
-echo '</ul>';
