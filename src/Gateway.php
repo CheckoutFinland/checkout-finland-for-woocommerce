a515a11b89c78e07b76281bd31f88e3d3dc1af06
Tomi Henttinen <tomi.henttinen@ambientia.fi>
2020-09-07 16:40:58 +0300

    cleanup

--- src/Gateway.php
+++ src/Gateway.php
@@ -1044,10 +1044,6 @@ final class Gateway extends \WC_Payment_Gateway
         // Set the order ID as the stamp to the payment request
         $payment->setStamp( get_current_blog_id() . '-' . $order->get_id() . '-' . time() );
 
-        // Create a random reference for the order
-        // Create a unique hash to be used as the request reference
-        //$reference = sha1( uniqid( true ) );
-
         // Use WooCom order number as reference
         // https://trello.com/c/i6GUZACP/83-reference-kentt%C3%A4%C3%A4n-woon-tilausnumero
         $reference = $order->get_order_number();
