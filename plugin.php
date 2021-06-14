6b7020d8ac6617cf99c86a3a9a9ef621dc2065c7
Joonas Loueranta <joonas.loueranta@op.fi>
2021-06-14 17:38:19 +0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,10 +5,10 @@
  * Description: Checkout Finland is a payment gateway that offers 20+ payment methods for Finnish customers.
  * Version: 1.6
  * Requires at least: 4.9
- * Tested up to: 5.6
+ * Tested up to: 5.7
  * Requires PHP: 7.3
  * WC requires at least: 3.0
- * WC tested up to: 4.9
+ * WC tested up to: 5.3
  * Author: Checkout Finland
  * Author URI: https://www.checkout.fi/
  * Text Domain: op-payment-service-for-woocommerce
